723231cbbb6bb02f8f232dae637312b9a6f6a689
mikaelmoutakis <mikaelmoutakis@gmail.com>
2020-07-24 12:21:18 +0200

    second working version

--- expenses.py
+++ expenses.py
@@ -1,16 +1,18 @@
+#!/home/mikael/.local/bin/streamlit run
 import streamlit as st
 import requests
 import pandas as pd
 from pathlib import Path
 from datetime import datetime
+import configparser
+import numpy as np
+import sys
 
 
-def importer(address, port, output_directory="."):
-    # download csv-file
-    # split file
-    # read expenses as pandas file
-    # clean up data
-    # create feather file
+def importer(address, port, priorities, feather_file_path):
+    """Downloads the Report.csv file from the HomeBudget
+    app, extracts the list of expenses and saves them to a
+    feather file in the data/ directory"""
     url = f"http://{address}:{port}/Report.csv"
     report = requests.get(url)
     expenses, *incomes_and_sources = report.text.split("\n\n")
@@ -18,7 +20,7 @@ def importer(address, port, output_directory="."):
         expenses_csv.write(expenses)
     with open("/tmp/expenses.csv", "r") as expenses_csv:
         expenses_pd = pd.read_csv(expenses_csv)
-    #data cleaning
+    # data cleaning
     expenses_pd.columns = [
         "date",
         "category",
@@ -34,70 +36,165 @@ def importer(address, port, output_directory="."):
     expenses_pd["year"] = expenses_pd["date"].dt.year
     expenses_pd["month"] = expenses_pd["date"].dt.month
     expenses_pd["week"] = expenses_pd["date"].dt.week
-    expenses_pd.to_feather(Path.joinpath(Path(output_directory), "expenses.feather"))
+    expenses_pd["subcategory_lower"] = expenses_pd.subcategory.str.lower()
+    expenses_pd = expenses_pd.merge(
+        priorities, on="subcategory_lower"
+    )
+    expenses_pd.to_feather(feather_file_path)
     return expenses_pd
 
 
+def agg_over_categories(df_org):
+    """Aggregates expenses over categories"""
+    pivot = pd.pivot_table(
+        df_org,
+        values="amount",
+        columns="year",
+        index="category",
+        fill_value=0,
+        margins=True,
+        aggfunc=np.sum,
+    ).iloc[:, :-1]
+    pivot["Change (%)"] = (pivot.iloc[:, 1] / pivot.iloc[:, 0] - 1) * 100
+    return pivot.round(0)
+
+
+def agg_over_subcategories(df_org, selected_subcategories):
+    """Aggregates expenses over subcategories"""
+    df = df_org.set_index("subcategory").loc[selected_subcategories].reset_index()
+    pivot = pd.pivot_table(
+        df,
+        values="amount",
+        columns="year",
+        index="subcategory",
+        fill_value=0,
+        margins=True,
+        aggfunc=np.sum,
+    ).iloc[:, :-1]
+    pivot["Change (%)"] = (pivot.iloc[:, 1] / pivot.iloc[:, 0] - 1) * 100
+    return pivot.round(0)
+
+
+def agg_over_prio(df_org, selected_priorities):
+    """Aggregates expenses over priorities"""
+    df = df_org.set_index("priority").loc[selected_priorities].reset_index()
+    pivot = pd.pivot_table(
+        df,
+        values="amount",
+        columns="year",
+        index="priority",
+        fill_value=0,
+        margins=True,
+        aggfunc=np.sum,
+    ).iloc[:, :-1]
+    pivot["Change (%)"] = (pivot.iloc[:, 1] / pivot.iloc[:, 0] - 1) * 100
+    return pivot.round(0)
 
 
 ###################
 ### main script ###
 ###################
 
-st.title("Expenses")
+if __name__ == "__main__":
+    try:
+        config_file = sys.argv[1]
+    except IndexError:
+        print("Error: run './expenses.py path-to-config.ini'")
+        sys.exit(1)
+    ### configs ###
+    config_file = Path(config_file)
+    if not config_file.exists():
+        print(f"Error: {config_file} does not exist")
+        sys.exit(1)
+    config = configparser.ConfigParser()
+    config.read(config_file)
 
-#############
-## sidebar ##
-#############
+    #############
+    st.title("Expenses")
 
-port = st.sidebar.text_input("Host port")
-import_button = st.sidebar.button("Import")
+    #############
+    ## sidebar ##
+    #############
+    host = st.sidebar.text_input("Smartphone address", config["CONFIGS"]["host"])
+    port = st.sidebar.text_input("HomeBudget port")
+    import_button = st.sidebar.button("Import from phone")
 
-if import_button:
-    expenses = importer("woland.moutakis.name",port)
-    #import_button = st.sidebar.button("Import again")
-else:
-    expenses = pd.read_feather("expenses.feather")
+    #### data import ###
+    expenses_feather_file = Path.joinpath(
+        Path(config["CONFIGS"]["datadir"]), "expenses.feather"
+    )
+    if import_button:
+        priorities = pd.DataFrame(
+            {
+                "subcategory_lower": list(config["SUBCATEGORIES"].keys()),
+                "priority": list(config["SUBCATEGORIES"].values()),
+            }
+        )
+        expenses = importer(host, port, priorities, expenses_feather_file)
+        st.sidebar.info("Updated database")
+    else:
+        expenses = pd.read_feather(expenses_feather_file)
+        ####################
 
-categories = ["ALL"] + list(expenses.category.unique())
+    ######################
+    ##### side menue #####
+    ######################
+    today = datetime.now()
+    expenses_years = list(expenses["year"].unique())
+    year_index_1 = len(expenses_years) - 2
+    year_index_2 = year_index_1 + 1
+    last_year = int(st.sidebar.selectbox("First year", expenses_years, year_index_1))
+    this_year = int(st.sidebar.selectbox("Second year", expenses_years, year_index_2))
 
-chosen_category = st.sidebar.selectbox("Choose a category",
-                                       categories)
+    this_month = today.month
+    current_months = range(
+        1, this_month
+    )  # todo: range(min_month_current_year, max_month_current_year)
+    month_list = st.sidebar.multiselect(
+        "Months:", current_months, default=current_months
+    )
 
-aggregate_over = st.sidebar.radio("Aggregate over",["years","months"])
+    agg_type = st.sidebar.radio(
+        "Aggregate over: ", ["categories", "subcategories", "priorities"], index=0
+    )
 
-###########
-## main ###
-###########
-today = datetime.now()
-this_year = today.year
-last_year = this_year-1
-this_month = today.month
+    expenses = expenses.set_index("year").loc[[last_year, this_year]].reset_index()
+    expenses = expenses.set_index("month").loc[month_list].reset_index()
 
-expenses = expenses.query(f"year>={last_year}").query(f"month<{this_month}")
-min_date = min(expenses.date)
-max_date = max(expenses.date)
+    # dynamic menue
+    if agg_type == "subcategories":
+        subcat_list = list(expenses.subcategory.unique())
+        agg_list = st.sidebar.multiselect(
+            "Subcategories:", subcat_list, default=subcat_list
+        )
+    elif agg_type == "priorities":
+        prio_list = list(set(config["SUBCATEGORIES"].values()))
+        agg_list = st.sidebar.multiselect(
+            "Priority levels:", prio_list, default=prio_list
+        )
+    else:
+        #agg_type == "categories":
+        agg_list = None
 
-if chosen_category!="ALL":
-    expenses = expenses.query(f"category=='{chosen_category}'")
+    ###########
+    ## main ###
+    ###########
+    min_date = min(expenses.date)
+    max_date = max(expenses.date)
 
-if aggregate_over=="years":
-    expenses_grouped = expenses[["year", "category", "subcategory", "amount"]]
-    totals = expenses_grouped.groupby(["year", "category", "subcategory"]).sum().round(0)
-else:
-    expenses_grouped = expenses[["year", "month", "category", "subcategory", "amount"]]
-    totals = expenses_grouped.groupby(["year", "month","category", "subcategory"]).sum().round(0)
+    #### presentation ####
+    st.info(
+        f"Data from {min_date.strftime('%Y-%m-%d')} to {max_date.strftime('%Y-%m-%d')}"
+    )
 
-
-
-
-totals2 = totals.unstack("year",fill_value=0)
-
-#print(totals2.dtypes)
-#print(totals2.diff(axis=1))
-
-
-
-#### presentation ####
-st.info(f"Data from {min_date} to {max_date}")
-st.dataframe(totals2)
+    if agg_type == "subcategories":
+        out_table = agg_over_subcategories(
+            df_org=expenses, selected_subcategories=agg_list
+        )
+        st.table(out_table)
+    elif agg_type == "priorities":
+        out_table = agg_over_prio(df_org=expenses, selected_priorities=agg_list)
+        st.table(out_table)
+    elif agg_type == "categories":
+        out_table = agg_over_categories(df_org=expenses)
+        st.table(out_table)
